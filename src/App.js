38cbf13b5122afc120539f413e77f5a0da6028ff
arinavictor <arinavictor@yahoo.com>
2019-11-06 09:53:32 -0700

    add a header and a main, populated them, and styled

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+        <header>
+        <h1>Bagel Shop</h1>
+        </header>
+        <main>
+            <h1>Coming soon!</h1>
+        </main>
     </div>
   );
 }
